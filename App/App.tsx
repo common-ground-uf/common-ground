1300be81429dbfb6e8ebda2a362f72c21e3a2588
Eric Navar <ericnavar@ufl.edu>
2022-09-05 15:31:06 -0400

    add screen for restaurant profile

--- App/App.tsx
+++ App/App.tsx
@@ -3,7 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from './screens/Login';
 import { Signup } from './screens/Signup';
 import { ForgorPassword } from './screens/ForgorPassword💀';
+import { RestaurantScreen } from './screens/RestaurantScreen';
 import React from 'react';
+import {tacoBell} from './data/dummyData';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} initialParams={{restaurant:tacoBell}}/>
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Forgor Password 💀" component={ForgorPassword} />
---
+++ App/data/dummyData.ts
@@ -0,0 +1,21 @@
+export const tacoBell = {
+  name: 'Taco Bell',
+  thumbnail: 'https://www.tacobell.com/medias/blog-2019-new-menu-header.jpg?context=bWFzdGVyfGltYWdlc3wxNjYyNzd8aW1hZ2UvanBlZ3xpbWFnZXMvaDFhL2g5OC84OTI4NTEyMTE0NzE4LmpwZ3xjMjA5YzZjNTQyMmM0MWM1ZWE5N2Q1OGQ0NDJjYWVkMzdiNTE0MjMyMjRhMzhmMTZjNGEwYTc4YmMwOGYwOWYx',
+  address: {
+    line1:  '1515 SW 13th Street',
+    line2: 'Gainesville, FL 32601'
+  },
+  priceRating: 1,
+  starRating: 3,
+  description: 'Fast-food chain serving Mexican-inspired fare such as tacos, quesadillas & nachos.',
+  reviews: [
+    {
+      starRating: 1,
+      bodyText: 'It was ok.'
+    },
+    {
+      starRating: 5,
+      bodyText: 'The quesalupa is good. I ordered 5 of them and they were yummy and I ate all of them.'
+    }
+  ]
+};
