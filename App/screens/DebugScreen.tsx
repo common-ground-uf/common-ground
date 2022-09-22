898cda47163dcc4355dc112e09f4be2941115a71
jeffreysf <jeffreysf1724@gmail.com>
2022-09-22 12:05:29 -0400

    added settings to debug screen

--- App/screens/DebugScreen.tsx
+++ App/screens/DebugScreen.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import {View, Button} from 'react-native';
 
 type DebugScreenProps = {
-  navigation: any;
+    navigation: any;
 }
 
 function DebugScreen(props: DebugScreenProps) {
-  const onClick = (screen: string) => {
-    props.navigation.navigate(screen);
-  };
-  return (
-    <View>
-      <Button title="Login" onPress={() => onClick('Login')} color="#FEB346" />
-      <Button title="Signup" onPress={() => onClick('Signup')} color="#FEB346" />
-      <Button title="Forgor Password 💀" onPress={() => onClick('Forgor Password 💀')} color="#FEB346" />
-      <Button title="Restaurant" onPress={() => onClick('Restaurant')} color="#FEB346" />
-      <Button title="Restaurant List" onPress={() => onClick('Restaurant List')} color="#FEB346" />
-      <Button title="Profile" onPress={() => onClick('Profile')} color="#FEB346" />
-      <Button title="Home" onPress={() => onClick('Home')} color="#FEB346" />
-      <Button title="GroupDetails" onPress={() => onClick('GroupDetails')} color="#FEB346" />
-    </View>
-  );
+    const onClick = (screen: string) => {
+        props.navigation.navigate(screen);
+    };
+    return (
+        <View>
+            <Button title="Login" onPress={() => onClick('Login')} color="#FEB346"/>
+            <Button title="Signup" onPress={() => onClick('Signup')} color="#FEB346"/>
+            <Button title="Forgor Password 💀" onPress={() => onClick('Forgor Password 💀')} color="#FEB346"/>
+            <Button title="Restaurant" onPress={() => onClick('Restaurant')} color="#FEB346"/>
+            <Button title="Restaurant List" onPress={() => onClick('Restaurant List')} color="#FEB346"/>
+            <Button title="Profile" onPress={() => onClick('Profile')} color="#FEB346"/>
+            <Button title="Settings" onPress={() => onClick('Settings')} color="#FEB346"/>
+            <Button title="Account" onPress={() => onClick('Account')} color="#FEB346"/>
+            <Button title="Privacy" onPress={() => onClick('Privacy')} color="#FEB346"/>
+            <Button title="Notifications" onPress={() => onClick('Notifications')} color="#FEB346"/>
+            <Button title="Home" onPress={() => onClick('Home')} color="#FEB346"/>
+            <Button title="GroupDetails" onPress={() => onClick('GroupDetails')} color="#FEB346"/>
+        </View>
+    );
 }
 
-export { DebugScreen };
+export {DebugScreen};
