9d9f7246adbec4aa3d69c0ab5df8d2ab30e0aa0b
jeffreysf <jeffreysf1724@gmail.com>
2022-09-29 03:48:41 -0400

    changed name of screen to join group

--- App/App.tsx
+++ App/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
                 <Stack.Screen name="Messages" component={screens.MessagesScreen}/>
                 <Stack.Screen name="StartNewTableScreen" component={screens.StartNewTableScreen}/>
                 <Stack.Screen name="ResetPassword" component={screens.ResetPassword}/>
-                <Stack.Screen name="JoinGroup" component={screens.JoinGroup}/>
+                <Stack.Screen name="Join Group" component={screens.JoinGroup}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
