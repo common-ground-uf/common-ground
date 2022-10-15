07f1013895596c8b7e94de4bd0ab047c1ed87eef
Ranger Chenore <r2chenore@gmail.com>
2022-10-15 14:08:39 -0400

    Uncomment yelp test code

--- App/api/yelpApi.test.ts
+++ App/api/yelpApi.test.ts
@@ -6,297 +6,297 @@ I'll make this work later by configuring circle-ci to use a yelp api key that I'
 
 */
 
-import {describe, expect, test} from '@jest/globals';
+// import {describe, expect, test} from '@jest/globals';
 
-describe('Test test', () => {
-  test('ADD ACTUAL TESTS FOR PriceRating.tsx HERE', () => {
-    expect(true).toBe(true);
-  });
+// describe('Test test', () => {
+//   test('ADD ACTUAL TESTS FOR PriceRating.tsx HERE', () => {
+//     expect(true).toBe(true);
+//   });
+// });
+
+
+import { describe, expect, test } from '@jest/globals';
+import { getBusinessDetails, getBusinessesByLocation, Business, BusinessDetails } from './yelpApi';
+
+let tacoBellExample: Business = null;
+let businessDetailExample: BusinessDetails = null;
+
+beforeAll(async () => {
+    
+    let val0 = await getBusinessDetails('M_Df54iwfPrQhWqAUBf5Kw');
+    businessDetailExample = val0;
+
+    let val = await getBusinessesByLocation('Gainesville, Florida');
+    console.log("Taco bell example " + JSON.stringify(val));
+    tacoBellExample = val[0];
+    return val;
 });
 
+// test existence of all above attributes
+describe('Business attributes exist as expected', () => {
+    test('rating', () => {
+        expect(tacoBellExample).toHaveProperty('rating');
+    });
+    test('price', () => {
+        expect(tacoBellExample).toHaveProperty('price');
+    });
+    test('phone', () => {
+        expect(tacoBellExample).toHaveProperty('phone');
+    });
+    test('id', () => {
+        expect(tacoBellExample).toHaveProperty('id');
+    });
+    test('alias', () => {
+        expect(tacoBellExample).toHaveProperty('alias');
+    });
+    test('is_closed', () => {
+        expect(tacoBellExample).toHaveProperty('is_closed');
+    });
+    test('categories', () => {
+        expect(tacoBellExample).toHaveProperty('categories');
+    });
+    test('review_count', () => {
+        expect(tacoBellExample).toHaveProperty('review_count');
+    });
+    test('name', () => {
+        expect(tacoBellExample).toHaveProperty('name');
+    });
+    test('url', () => {
+        expect(tacoBellExample).toHaveProperty('url');
+    });
+    test('coordinates', () => {
+        expect(tacoBellExample).toHaveProperty('coordinates');
+    });
+    test('image_url', () => {
+        expect(tacoBellExample).toHaveProperty('image_url');
+    });
+    test('location', () => {
+        expect(tacoBellExample).toHaveProperty('location');
+    });
+    test('distance', () => {
+        expect(tacoBellExample).toHaveProperty('distance');
+    });
+    test('transactions', () => {
+        expect(tacoBellExample).toHaveProperty('transactions');
+    });
+});
 
-// import { describe, expect, test } from '@jest/globals';
-// import { getBusinessDetails, getBusinessesByLocation, Business, BusinessDetails } from './yelpApi';
+// test types of all above attributes
+describe('Business attributes have expected types', () => {
+    test('rating', () => {
+        expect(typeof tacoBellExample.rating).toBe('number');
+    });
+    test('price', () => {
+        expect(typeof tacoBellExample.price).toBe('string');
+    });
+    test('phone', () => {
+        expect(typeof tacoBellExample.phone).toBe('string');
+    });
+    test('id', () => {
+        expect(typeof tacoBellExample.id).toBe('string');
+    });
+    test('alias', () => {
+        expect(typeof tacoBellExample.alias).toBe('string');
+    });
+    test('is_closed', () => {
+        expect(typeof tacoBellExample.is_closed).toBe('boolean');
+    });
+    test('categories', () => {
+        expect(Array.isArray(tacoBellExample.categories)).toBe(true);
+    });
+    test('review_count', () => {
+        expect(typeof tacoBellExample.review_count).toBe('number');
+    });
+    test('name', () => {
+        expect(typeof tacoBellExample.name).toBe('string');
+    });
+    test('url', () => {
+        expect(typeof tacoBellExample.url).toBe('string');
+    });
+    test('coordinates', () => {
+        expect(typeof tacoBellExample.coordinates).toBe('object');
+    });
+    test('image_url', () => {
+        expect(typeof tacoBellExample.image_url).toBe('string');
+    });
+    test('location', () => {
+        expect(typeof tacoBellExample.location).toBe('object');
+    });
+    test('distance', () => {
+        expect(typeof tacoBellExample.distance).toBe('number');
+    });
+    test('transactions', () => {
+        expect(Array.isArray(tacoBellExample.transactions)).toBe(true);
+    });
+});
 
-// let tacoBellExample: Business = null;
-// let businessDetailExample: BusinessDetails = null;
+/*
+export interface BusinessDetails {
+    id: string;
+    alias: string;
+    name: string;
+    image_url: string;
+    is_claimed: boolean;
+    is_closed: boolean;
+    url: string;
+    phone: string;
+    display_phone: string;
+    review_count: number;
+    categories: Array<{
+        alias: string;
+        title: string;
+    }>;
+    rating: number;
+    location: {
+        address1: string;
+        address2: string;
+        address3: string;
+        city: string;
+        zip_code: string;
+        country: string;
+        state: string;
+        display_address: Array<string>;
+        cross_streets: string;
+    };
+    coordinates: {
+        latitude: number;
+        longitude: number;
+    };
+    photos: Array<string>;
+    price: string;
+    hours: Array<{
+        open: Array<{
+            is_overnight: boolean;
+            start: string;
+            end: string;
+            day: number;
+        }>;
+        hours_type: string;
+        is_open_now: boolean;
+    }>;
+    transactions: Array<string>;
+    special_hours: Array<{
+        date: string;
+        is_closed: boolean;
+        start: string;
+        end: string;
+        is_overnight: boolean;
+    }>;
+}
+*/
 
-// beforeAll(async () => {
-    
-//     let val0 = await getBusinessDetails('M_Df54iwfPrQhWqAUBf5Kw');
-//     businessDetailExample = val0;
+// test that all above properties exist
+describe('BusinessDetails attributes exist as expected', () => {
+    test('id', () => {
+        expect(businessDetailExample).toHaveProperty('id');
+    });
+    test('alias', () => {
+        expect(businessDetailExample).toHaveProperty('alias');
+    });
+    test('name', () => {
+        expect(businessDetailExample).toHaveProperty('name');
+    });
+    test('image_url', () => {
+        expect(businessDetailExample).toHaveProperty('image_url');
+    });
+    test('is_claimed', () => {
+        expect(businessDetailExample).toHaveProperty('is_claimed');
+    });
+    test('is_closed', () => {
+        expect(businessDetailExample).toHaveProperty('is_closed');
+    });
+    test('url', () => {
+        expect(businessDetailExample).toHaveProperty('url');
+    });
+    test('phone', () => {
+        expect(businessDetailExample).toHaveProperty('phone');
+    });
+    test('display_phone', () => {
+        expect(businessDetailExample).toHaveProperty('display_phone');
+    });
+    test('review_count', () => {
+        expect(businessDetailExample).toHaveProperty('review_count');
+    });
+    test('categories', () => {
+        expect(businessDetailExample).toHaveProperty('categories');
+    });
+    test('rating', () => {
+        expect(businessDetailExample).toHaveProperty('rating');
+    });
+    test('location', () => {
+        expect(businessDetailExample).toHaveProperty('location');
+    });
+    test('coordinates', () => {
+        expect(businessDetailExample).toHaveProperty('coordinates');
+    });
+    test('photos', () => {
+        expect(businessDetailExample).toHaveProperty('photos');
+    });
+    test('price', () => {
+        expect(businessDetailExample).toHaveProperty('price');
+    });
+    test('hours', () => {
+        expect(businessDetailExample).toHaveProperty('hours');
+    });
+    test('transactions', () => {
+        expect(businessDetailExample).toHaveProperty('transactions');
+    });
+});
 
-//     let val = await getBusinessesByLocation('Gainesville, Florida');
-//     console.log("Taco bell example " + JSON.stringify(val));
-//     tacoBellExample = val[0];
-//     return val;
-// });
-
-// // test existence of all above attributes
-// describe('Business attributes exist as expected', () => {
-//     test('rating', () => {
-//         expect(tacoBellExample).toHaveProperty('rating');
-//     });
-//     test('price', () => {
-//         expect(tacoBellExample).toHaveProperty('price');
-//     });
-//     test('phone', () => {
-//         expect(tacoBellExample).toHaveProperty('phone');
-//     });
-//     test('id', () => {
-//         expect(tacoBellExample).toHaveProperty('id');
-//     });
-//     test('alias', () => {
-//         expect(tacoBellExample).toHaveProperty('alias');
-//     });
-//     test('is_closed', () => {
-//         expect(tacoBellExample).toHaveProperty('is_closed');
-//     });
-//     test('categories', () => {
-//         expect(tacoBellExample).toHaveProperty('categories');
-//     });
-//     test('review_count', () => {
-//         expect(tacoBellExample).toHaveProperty('review_count');
-//     });
-//     test('name', () => {
-//         expect(tacoBellExample).toHaveProperty('name');
-//     });
-//     test('url', () => {
-//         expect(tacoBellExample).toHaveProperty('url');
-//     });
-//     test('coordinates', () => {
-//         expect(tacoBellExample).toHaveProperty('coordinates');
-//     });
-//     test('image_url', () => {
-//         expect(tacoBellExample).toHaveProperty('image_url');
-//     });
-//     test('location', () => {
-//         expect(tacoBellExample).toHaveProperty('location');
-//     });
-//     test('distance', () => {
-//         expect(tacoBellExample).toHaveProperty('distance');
-//     });
-//     test('transactions', () => {
-//         expect(tacoBellExample).toHaveProperty('transactions');
-//     });
-// });
-
-// // test types of all above attributes
-// describe('Business attributes have expected types', () => {
-//     test('rating', () => {
-//         expect(typeof tacoBellExample.rating).toBe('number');
-//     });
-//     test('price', () => {
-//         expect(typeof tacoBellExample.price).toBe('string');
-//     });
-//     test('phone', () => {
-//         expect(typeof tacoBellExample.phone).toBe('string');
-//     });
-//     test('id', () => {
-//         expect(typeof tacoBellExample.id).toBe('string');
-//     });
-//     test('alias', () => {
-//         expect(typeof tacoBellExample.alias).toBe('string');
-//     });
-//     test('is_closed', () => {
-//         expect(typeof tacoBellExample.is_closed).toBe('boolean');
-//     });
-//     test('categories', () => {
-//         expect(Array.isArray(tacoBellExample.categories)).toBe(true);
-//     });
-//     test('review_count', () => {
-//         expect(typeof tacoBellExample.review_count).toBe('number');
-//     });
-//     test('name', () => {
-//         expect(typeof tacoBellExample.name).toBe('string');
-//     });
-//     test('url', () => {
-//         expect(typeof tacoBellExample.url).toBe('string');
-//     });
-//     test('coordinates', () => {
-//         expect(typeof tacoBellExample.coordinates).toBe('object');
-//     });
-//     test('image_url', () => {
-//         expect(typeof tacoBellExample.image_url).toBe('string');
-//     });
-//     test('location', () => {
-//         expect(typeof tacoBellExample.location).toBe('object');
-//     });
-//     test('distance', () => {
-//         expect(typeof tacoBellExample.distance).toBe('number');
-//     });
-//     test('transactions', () => {
-//         expect(Array.isArray(tacoBellExample.transactions)).toBe(true);
-//     });
-// });
-
-// /*
-// export interface BusinessDetails {
-//     id: string;
-//     alias: string;
-//     name: string;
-//     image_url: string;
-//     is_claimed: boolean;
-//     is_closed: boolean;
-//     url: string;
-//     phone: string;
-//     display_phone: string;
-//     review_count: number;
-//     categories: Array<{
-//         alias: string;
-//         title: string;
-//     }>;
-//     rating: number;
-//     location: {
-//         address1: string;
-//         address2: string;
-//         address3: string;
-//         city: string;
-//         zip_code: string;
-//         country: string;
-//         state: string;
-//         display_address: Array<string>;
-//         cross_streets: string;
-//     };
-//     coordinates: {
-//         latitude: number;
-//         longitude: number;
-//     };
-//     photos: Array<string>;
-//     price: string;
-//     hours: Array<{
-//         open: Array<{
-//             is_overnight: boolean;
-//             start: string;
-//             end: string;
-//             day: number;
-//         }>;
-//         hours_type: string;
-//         is_open_now: boolean;
-//     }>;
-//     transactions: Array<string>;
-//     special_hours: Array<{
-//         date: string;
-//         is_closed: boolean;
-//         start: string;
-//         end: string;
-//         is_overnight: boolean;
-//     }>;
-// }
-// */
-
-// // test that all above properties exist
-// describe('BusinessDetails attributes exist as expected', () => {
-//     test('id', () => {
-//         expect(businessDetailExample).toHaveProperty('id');
-//     });
-//     test('alias', () => {
-//         expect(businessDetailExample).toHaveProperty('alias');
-//     });
-//     test('name', () => {
-//         expect(businessDetailExample).toHaveProperty('name');
-//     });
-//     test('image_url', () => {
-//         expect(businessDetailExample).toHaveProperty('image_url');
-//     });
-//     test('is_claimed', () => {
-//         expect(businessDetailExample).toHaveProperty('is_claimed');
-//     });
-//     test('is_closed', () => {
-//         expect(businessDetailExample).toHaveProperty('is_closed');
-//     });
-//     test('url', () => {
-//         expect(businessDetailExample).toHaveProperty('url');
-//     });
-//     test('phone', () => {
-//         expect(businessDetailExample).toHaveProperty('phone');
-//     });
-//     test('display_phone', () => {
-//         expect(businessDetailExample).toHaveProperty('display_phone');
-//     });
-//     test('review_count', () => {
-//         expect(businessDetailExample).toHaveProperty('review_count');
-//     });
-//     test('categories', () => {
-//         expect(businessDetailExample).toHaveProperty('categories');
-//     });
-//     test('rating', () => {
-//         expect(businessDetailExample).toHaveProperty('rating');
-//     });
-//     test('location', () => {
-//         expect(businessDetailExample).toHaveProperty('location');
-//     });
-//     test('coordinates', () => {
-//         expect(businessDetailExample).toHaveProperty('coordinates');
-//     });
-//     test('photos', () => {
-//         expect(businessDetailExample).toHaveProperty('photos');
-//     });
-//     test('price', () => {
-//         expect(businessDetailExample).toHaveProperty('price');
-//     });
-//     test('hours', () => {
-//         expect(businessDetailExample).toHaveProperty('hours');
-//     });
-//     test('transactions', () => {
-//         expect(businessDetailExample).toHaveProperty('transactions');
-//     });
-// });
-
-// // test that all above properties have expected types
-// describe('BusinessDetails attributes have expected types', () => {
-//     test('id', () => {
-//         expect(typeof businessDetailExample.id).toBe('string');
-//     });
-//     test('alias', () => {
-//         expect(typeof businessDetailExample.alias).toBe('string');
-//     });
-//     test('name', () => {
-//         expect(typeof businessDetailExample.name).toBe('string');
-//     });
-//     test('image_url', () => {
-//         expect(typeof businessDetailExample.image_url).toBe('string');
-//     });
-//     test('is_claimed', () => {
-//         expect(typeof businessDetailExample.is_claimed).toBe('boolean');
-//     });
-//     test('is_closed', () => {
-//         expect(typeof businessDetailExample.is_closed).toBe('boolean');
-//     });
-//     test('url', () => {
-//         expect(typeof businessDetailExample.url).toBe('string');
-//     });
-//     test('phone', () => {
-//         expect(typeof businessDetailExample.phone).toBe('string');
-//     });
-//     test('display_phone', () => {
-//         expect(typeof businessDetailExample.display_phone).toBe('string');
-//     });
-//     test('review_count', () => {
-//         expect(typeof businessDetailExample.review_count).toBe('number');
-//     });
-//     test('categories', () => {
-//         expect(Array.isArray(businessDetailExample.categories)).toBe(true);
-//     });
-//     test('rating', () => {
-//         expect(typeof businessDetailExample.rating).toBe('number');
-//     });
-//     test('location', () => {
-//         expect(typeof businessDetailExample.location).toBe('object');
-//     });
-//     test('coordinates', () => {
-//         expect(typeof businessDetailExample.coordinates).toBe('object');
-//     });
-//     test('photos', () => {
-//         expect(Array.isArray(businessDetailExample.photos)).toBe(true);
-//     });
-//     test('price', () => {
-//         expect(typeof businessDetailExample.price).toBe('string');
-//     });
-//     test('hours', () => {
-//         expect(Array.isArray(businessDetailExample.hours)).toBe(true);
-//     });
-//     test('transactions', () => {
-//         expect(Array.isArray(businessDetailExample.transactions)).toBe(true);
-//     });
-// });
+// test that all above properties have expected types
+describe('BusinessDetails attributes have expected types', () => {
+    test('id', () => {
+        expect(typeof businessDetailExample.id).toBe('string');
+    });
+    test('alias', () => {
+        expect(typeof businessDetailExample.alias).toBe('string');
+    });
+    test('name', () => {
+        expect(typeof businessDetailExample.name).toBe('string');
+    });
+    test('image_url', () => {
+        expect(typeof businessDetailExample.image_url).toBe('string');
+    });
+    test('is_claimed', () => {
+        expect(typeof businessDetailExample.is_claimed).toBe('boolean');
+    });
+    test('is_closed', () => {
+        expect(typeof businessDetailExample.is_closed).toBe('boolean');
+    });
+    test('url', () => {
+        expect(typeof businessDetailExample.url).toBe('string');
+    });
+    test('phone', () => {
+        expect(typeof businessDetailExample.phone).toBe('string');
+    });
+    test('display_phone', () => {
+        expect(typeof businessDetailExample.display_phone).toBe('string');
+    });
+    test('review_count', () => {
+        expect(typeof businessDetailExample.review_count).toBe('number');
+    });
+    test('categories', () => {
+        expect(Array.isArray(businessDetailExample.categories)).toBe(true);
+    });
+    test('rating', () => {
+        expect(typeof businessDetailExample.rating).toBe('number');
+    });
+    test('location', () => {
+        expect(typeof businessDetailExample.location).toBe('object');
+    });
+    test('coordinates', () => {
+        expect(typeof businessDetailExample.coordinates).toBe('object');
+    });
+    test('photos', () => {
+        expect(Array.isArray(businessDetailExample.photos)).toBe(true);
+    });
+    test('price', () => {
+        expect(typeof businessDetailExample.price).toBe('string');
+    });
+    test('hours', () => {
+        expect(Array.isArray(businessDetailExample.hours)).toBe(true);
+    });
+    test('transactions', () => {
+        expect(Array.isArray(businessDetailExample.transactions)).toBe(true);
+    });
+});
