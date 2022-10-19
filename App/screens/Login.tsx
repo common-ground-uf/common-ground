ffbdb3c4cb47b7a358b726ae50f4888d9bf7bffd
jeffreysf <jeffreysf1724@gmail.com>
2022-10-19 18:42:43 -0400

    connected login frontend to backend

--- App/screens/Login.tsx
+++ App/screens/Login.tsx
@@ -1,51 +1,93 @@
 import React from 'react';
-import { Text, View, TextInput, Button  } from 'react-native';
+import {Text, View, TextInput, Button} from 'react-native';
 import {loginSignupStyles} from '../styles/LoginSingup';
 
+const axios = require('axios');
+
 type LoginProps = {
-  navigation: any;
+    navigation: any;
+}
+
+type requestLoginProps = {
+    email: string,
+    password: string
 }
 
 function Login(props: LoginProps) {
-  const [email, setEmail] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
-  
-  const onPressForgotPassword = () => {
-    props.navigation.navigate('Forgot Password');
-  };
+    const [email, setEmail] = React.useState<string>('');
+    const [password, setPassword] = React.useState<string>('');
 
-  const onPressLogin = () => {
-    console.log('log in');
-  };
 
-  const onPressSignUp = () => {
-    props.navigation.navigate('Signup');
-  };
+    const onPressForgotPassword = () => {
+        props.navigation.navigate('Forgot Password');
+    };
 
-  return (
-    <View style={loginSignupStyles.container}>
-      <Text style={loginSignupStyles.title}>Login</Text>
-      <TextInput
-        style={loginSignupStyles.input}
-        onChangeText={setEmail}
-        value={email}
-        placeholder="Email"
+    const onPressLogin = () => {
+        axios.post('http://192.168.86.93:3000/login', {username: email, password: password})
+            .then(response => {
+                if(response.data.message === "login success") {
+                    console.log("login successful");
+                    // User Data object to be processed locally and saved as current login data (cleared after logout)
+                    const userData = {
+                        email: response.data.userData.email,
+                        firstname: response.data.userData.firstname,
+                        lastname: response.data.userData.lastname,
+                        role: response.data.userData.role,
+                    }
+                    console.log(userData);
+                }
+            })
+            .catch((error)=> {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    if(error.response.data.message === "login failed")
+                        console.log("login unsuccessful")
+                    else {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    }
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+            })
+    };
 
-      />
-      <TextInput
-        style={loginSignupStyles.input}
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Password"
-        autoComplete='password'
-      />
-      <Text onPress={onPressForgotPassword} style={loginSignupStyles.link}>Forgot password</Text>
-      <Text onPress={onPressSignUp} style={loginSignupStyles.link}>Sign up</Text>
-      <View style={loginSignupStyles.loginButtonContainer}>
-        <Button title="Login" onPress={onPressLogin} color="#FEB346"/>
-      </View>
-    </View>
-  );
+    const onPressSignUp = () => {
+        props.navigation.navigate('Signup');
+    };
+
+    return (
+        <View style={loginSignupStyles.container}>
+            <Text style={loginSignupStyles.title}>Login</Text>
+            <TextInput
+                style={loginSignupStyles.input}
+                onChangeText={setEmail}
+                value={email}
+                placeholder="Email"
+
+            />
+            <TextInput
+                style={loginSignupStyles.input}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Password"
+                autoComplete='password'
+            />
+            <Text onPress={onPressForgotPassword} style={loginSignupStyles.link}>Forgot password</Text>
+            <Text onPress={onPressSignUp} style={loginSignupStyles.link}>Sign up</Text>
+            <View style={loginSignupStyles.loginButtonContainer}>
+                <Button title="Login" onPress={onPressLogin} color="#FEB346"/>
+            </View>
+        </View>
+    );
 }
 
-export { Login };
+export {Login};
