49bdb5b9ba2964f5583ac485dcc886d5d15ccb03
Eric Navar <ericnavar@ufl.edu>
2022-10-30 18:40:57 -0400

    add precommit

---
+++ App/screens/ProfileScreen.spec.tsx
@@ -0,0 +1,44 @@
+import { saulProfile, walterProfile } from "../data/dummyUsers";
+import { ProfileScreen } from "./ProfileScreen";
+import { render } from "@testing-library/react-native";
+import React from "react";
+
+describe("ProfileScreen", () => {
+  const dummyNavigation = {
+    navigate: "",
+  };
+
+  it("Renders user's own profile correctly", () => {
+    const route = {
+      params: {
+        profileData: saulProfile,
+      },
+    };
+    const result = render(
+      <ProfileScreen
+        profileData={saulProfile}
+        navigation={dummyNavigation}
+        route={route}
+      />
+    );
+
+    expect(result.toJSON()).toMatchSnapshot();
+  });
+
+  it("Renders someone else's profile correctly", () => {
+    const route = {
+      params: {
+        profileData: walterProfile,
+      },
+    };
+    const result = render(
+      <ProfileScreen
+        profileData={walterProfile}
+        navigation={dummyNavigation}
+        route={route}
+      />
+    );
+
+    expect(result.toJSON()).toMatchSnapshot();
+  });
+});
