bd0b4479c122e73826a83084aa70147bf3c8da47
Hirshey <noah@thehirschfields.com>
2022-10-31 18:59:24 -0400

    add login screen text and change testing version

---
+++ App/screens/Login.test.tsx
@@ -0,0 +1,45 @@
+// import {describe, expect, test} from '@jest/globals';
+import { Login } from './Login';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
+import React from 'react';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+describe('Login screen', () => {
+  test('Login request', async () => {
+
+    // test user creds
+    const email = "testuser@gmail.com"
+    const password = "admin"
+    let hasErr = false
+    
+    // get elements
+    const screen = render(<Login navigation={{
+        navigate: undefined,
+    }}  />);
+    const emailInput = screen.getByPlaceholderText('Email');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    // const passwordInput = screen.getByPlaceholderText('Password');
+    //get only button on page
+    const loginButton = screen.getAllByRole('button')[0];
+
+    try{
+        // fill in form
+        await waitFor(() => {
+            fireEvent.changeText(emailInput, email);
+            fireEvent.changeText(passwordInput, password);
+        });
+
+        //click submit button
+        await waitFor(() => {
+            fireEvent.press(loginButton);
+        });
+    }catch(e){
+        console.log(e);
+        hasErr = true
+    }
+
+    expect(hasErr).toBe(false);
+
+  });
+});
