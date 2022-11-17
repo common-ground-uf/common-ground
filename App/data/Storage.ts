e5ac566f14fb15ce3dc55970ed289a5a5b960cae
Hirshey <noah@thehirschfields.com>
2022-11-17 17:51:48 -0500

    add storage

---
+++ App/data/Storage.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const Storage = {
+    get: async (key: string) : Promise<string | null> => {
+        return await AsyncStorage.getItem(key);
+    },
+    set: async (key: string, value: string) => {
+        await AsyncStorage.setItem(key, value).then(() => {
+            console.log('Stored ', value, ' at ', key);
+        }).catch((error: any) => {
+            console.log('Error storing data at ', key, error);
+        })
+    },
+}
