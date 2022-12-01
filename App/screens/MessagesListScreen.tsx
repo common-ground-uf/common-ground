3b4eb10ef370308702e3b6ef0683d98790317b4b
Jeffrey Shim-Francis <27683122+jeffreysf@users.noreply.github.com>
2022-12-01 18:12:16 -0500

    messages lastmessage and other changes (#101)
    * navigation fixes, login stack
    
    * bug fix for groups list on home and axios error check navs to login
    
    * more login redirects
    
    * bug fix to lastmessage server side and client side render ui fix for messages and home

--- App/screens/MessagesListScreen.tsx
+++ App/screens/MessagesListScreen.tsx
@@ -1,126 +1,132 @@
 import React from 'react';
 import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  ScrollView,
+    View,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
 import axios from 'axios';
-import { SERVER_URI } from '../Config';
-import { GroupInfo } from '../commonTypes';
+import {SERVER_URI} from '../Config';
+import {GroupInfo} from '../commonTypes';
 
 const styles = StyleSheet.create({
-  inner: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'nowrap',
-    justifyContent: 'space-between',
-    minWidth: 0,
-    width: '100%',
-    maxWidth: '100%',
-    padding: 15,
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 16,
-  },
+    inner: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        marginTop: 20,
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between',
+        minWidth: 0,
+        width: '100%',
+        maxWidth: '100%',
+        padding: 15,
+        borderColor: '#000000',
+        borderBottomWidth: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    subtitle: {
+        fontSize: 16,
+    },
 });
 
 type MessageGroupProps = {
-  navigation: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    navigate: any;
-  };
-  id: string;
-  name: string;
-  lastMessage: string;
-  inviteCode: string;
+    navigation: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        navigate: any;
+    };
+    id: string;
+    name: string;
+    lastMessage: string;
+    inviteCode: string;
 };
 
-function GroupInList(props: MessageGroupProps){
-  const onClickGroup = () => {
-    props.navigation.navigate('MessagesScreen', {groupId: props.id, groupName: props.name, inviteCode: props.inviteCode});
-  };
+function GroupInList(props: MessageGroupProps) {
+    const onClickGroup = () => {
+        props.navigation.navigate('MessagesScreen', {
+            groupId: props.id,
+            groupName: props.name,
+            inviteCode: props.inviteCode
+        });
+    };
 
-  return (
-    <TouchableOpacity style={styles.row} onPress={onClickGroup}>
-      <Text style={styles.title}>{props.name}</Text>
-      <Text style={styles.subtitle}>{props.lastMessage}</Text>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity style={styles.row} onPress={onClickGroup}>
+            <Text style={styles.title}>{props.name}</Text>
+            <Text style={styles.subtitle}>{props.lastMessage}</Text>
+        </TouchableOpacity>
+    );
 }
 
 type MessagesListScreenProps = {
-  navigation: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    navigate: any;
-  };
+    navigation: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        navigate: any;
+    };
 }
 
 function MessagesListScreen(props: MessagesListScreenProps) {
-  //TODO: Change this to properly use auth to get self (not working for some reason)
+    //TODO: Change this to properly use auth to get self (not working for some reason)
 
-  const isFocused = useIsFocused();
+    const isFocused = useIsFocused();
 
-  const [groups, setGroups] = React.useState<GroupInfo[]>([]);
-  const updateGroups = async () => {
-    console.log('getting groups');
-    axios.get(`${SERVER_URI}/groups`, {
-      params: {
-        name: true,
-        lastMessage: true,
-        users: true
-      },
-    }).then((res) => {
-      const newGroups : GroupInfo[] = [];
-      for(const group in res.data.groups){
-        newGroups.push({
-          id: group,
-          name: res.data.groups[group].name,
-          lastMessage: res.data.groups[group].lastMessage,
-          inviteCode: res.data.groups[group].inviteCode,
+    const [groups, setGroups] = React.useState<GroupInfo[]>([]);
+    const updateGroups = async () => {
+        console.log('getting groups');
+        axios.get(`${SERVER_URI}/groups`, {
+            params: {
+                name: true,
+                lastMessage: true,
+                users: true
+            },
+        }).then((res) => {
+            // console.log(res.data.groups);
+
+            const newGroups: GroupInfo[] = [];
+            for (const group in res.data.groups) {
+                newGroups.push({
+                    id: group,
+                    name: res.data.groups[group].name,
+                    lastMessage: res.data.groups[group].lastMessage,
+                    inviteCode: res.data.groups[group].inviteCode,
+                });
+            }
+
+            if (JSON.stringify(newGroups) != JSON.stringify(groups))
+                setGroups(newGroups);
+            // setParties(res.data);
+        }).catch((err) => {
+            console.log(err);
+            props.navigation.navigate('Login');
         });
-      }
+    };
+    updateGroups();
 
-      if(JSON.stringify(newGroups)!=JSON.stringify(groups))
-        setGroups(newGroups);
-      // setParties(res.data);
-    }).catch((err) => {
-      console.log(err);
-      props.navigation.navigate('Login');
-    });
-  };
-  updateGroups();
-
-  return (
-    <ScrollView>
-      <View style={styles.inner}>
-        {groups.map((group) => 
-            <GroupInList
-              navigation={props.navigation}
-              id={group.id}
-              name={group.name}
-              lastMessage={group.lastMessage}
-              key={group.id}
-              inviteCode={group.inviteCode}
-            />
-          )}
-      </View>
-    </ScrollView>
-  );
+    return (
+        <ScrollView>
+            <View style={styles.inner}>
+                {groups.length === 0 ? [] : groups.map((group) =>
+                    <GroupInList
+                        navigation={props.navigation}
+                        id={group.id}
+                        name={group.name}
+                        lastMessage={group.lastMessage}
+                        key={group.id}
+                        inviteCode={group.inviteCode}
+                    />
+                )}
+            </View>
+        </ScrollView>
+    );
 }
 
-export { MessagesListScreen };
+export {MessagesListScreen};
