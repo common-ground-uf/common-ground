22cb8c932fc33ae469e9f396605d3aafad64f8f8
Eric Navar <eric.navar@asurion.com>
2022-12-01 01:06:57 -0500

    stuff

--- App/screens/Account.tsx
+++ App/screens/Account.tsx
@@ -13,38 +13,38 @@ function Account() {
   const [email, setEmail] = React.useState<string>('');
   const [location, setLocation] = React.useState<string>('');
 
-    axios.get(`${SERVER_URI}/auth`)
-        .then(response => {
-            if(response.data.message === 'login success') {
-                console.log('login successful');
-                // User Data object to be processed locally and saved as current login data (cleared after logout)
-                setEmail(response.data.userData.email);
-                setFirstName(response.data.userData.firstname);
-                setLastName(response.data.userData.lastname);
-               // TODO: Need to get location from user data
-            }
-        })
-        .catch((error)=> {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                if(error.response.data.message === 'not logged in')
-                    console.log('Not logged in!');
-                else {
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                }
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
-        });
+  axios.get(`${SERVER_URI}/auth`)
+      .then(response => {
+          if(response.data.message === 'login success') {
+              console.log('login successful');
+              // User Data object to be processed locally and saved as current login data (cleared after logout)
+              setEmail(response.data.userData.email);
+              setFirstName(response.data.userData.firstname);
+              setLastName(response.data.userData.lastname);
+              // TODO: Need to get location from user data
+          }
+      })
+      .catch((error)=> {
+          if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              if(error.response.data.message === 'not logged in')
+                  console.log('Not logged in!');
+              else {
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+              }
+          } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+          } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+          }
+      });
 
 
     const onPressSave = () => {
