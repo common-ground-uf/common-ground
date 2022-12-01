b911e59b9b7ed8a7c0d46e6ec92e283d567efb86
Eric Navar <eric.navar@asurion.com>
2022-11-30 22:16:39 -0500

    get profile pic in header from user profile

--- App/components/HeaderProfileIcon.tsx
+++ App/components/HeaderProfileIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, Image,TouchableOpacity} from 'react-native';
+import { Storage } from '../data/Storage';
 
 const styles = StyleSheet.create({
     image: {
@@ -10,20 +11,35 @@ const styles = StyleSheet.create({
 });
 
 type HeaderProfileIconProps = {
-    image: string;
     navigation: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         navigate: any;
     };
 };
 
 const HeaderProfileIcon = (props: HeaderProfileIconProps) => {
+    const [profileIcon, setProfileIcon] = React.useState();
+
+    const getProfileInfo = async () => {
+        //Get profile info from async storage
+        const profile = await Storage.get('profile');
+        if (profile) {
+          const profileInfo = JSON.parse(profile);
+          setProfileIcon(profileInfo.profilePic);
+        } else {
+          props.navigation.navigate('Login');
+        }
+    };
+    React.useEffect(() => {
+        getProfileInfo();
+    }, []);
     const onPress = () => {
         props.navigation.navigate('Profile');
     };
 
     return (
         <TouchableOpacity onPress={onPress}>
-            <Image style={styles.image} source={{uri: props.image}}/>
+            <Image style={styles.image} source={{uri: profileIcon}}/>
         </TouchableOpacity>
     );
 };
