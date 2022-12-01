92b21ad1549896825c5c80571037effff347cb6c
Eric Navar <eric.navar@asurion.com>
2022-12-01 13:13:03 -0500

    stuff

--- App/data/Storage.ts
+++ App/data/Storage.ts
@@ -4,11 +4,21 @@ export const Storage = {
     get: async (key: string) : Promise<string | null> => {
         return await AsyncStorage.getItem(key);
     },
-    set: async (key: string, value: string) => {
+    set: async (key: string, value?: string) => {
         await AsyncStorage.setItem(key, value).then(() => {
             console.log('Stored ', value, ' at ', key);
         }).catch((error: any) => {
             console.log('Error storing data at ', key, error);
         });
     },
+    delete: async (key: string) => {
+        try {
+            await AsyncStorage.removeItem(key);
+            console.log('deleted profile');
+            return true;
+        }
+        catch(exception) {
+            return false;
+        }
+    }
 };
