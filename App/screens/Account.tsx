dde46bf2b6979966079a764252e17d22fea0c6c0
Eric Navar <eric.navar@asurion.com>
2022-12-01 01:07:50 -0500

    stuff

--- App/screens/Account.tsx
+++ App/screens/Account.tsx
@@ -1,56 +1,55 @@
 import React from 'react';
-import {Button, TextInput, View} from 'react-native';
-import {loginSignupStyles} from '../styles/LoginSingup';
+import { Button, TextInput, View } from 'react-native';
+import { loginSignupStyles } from '../styles/LoginSingup';
 import axios from 'axios';
 import { SERVER_URI } from '../Config';
 
 function Account() {
-    // used to give initial values to the text fields
-
+  // used to give initial values to the text fields
 
   const [firstName, setFirstName] = React.useState<string>('');
   const [lastName, setLastName] = React.useState<string>('');
   const [email, setEmail] = React.useState<string>('');
   const [location, setLocation] = React.useState<string>('');
 
-  axios.get(`${SERVER_URI}/auth`)
-      .then(response => {
-          if(response.data.message === 'login success') {
-              console.log('login successful');
-              // User Data object to be processed locally and saved as current login data (cleared after logout)
-              setEmail(response.data.userData.email);
-              setFirstName(response.data.userData.firstname);
-              setLastName(response.data.userData.lastname);
-              // TODO: Need to get location from user data
-          }
-      })
-      .catch((error)=> {
-          if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              if(error.response.data.message === 'not logged in')
-                  console.log('Not logged in!');
-              else {
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
-              }
-          } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log(error.request);
-          } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-          }
-      });
+  axios
+    .get(`${SERVER_URI}/auth`)
+    .then((response) => {
+      if (response.data.message === 'login success') {
+        console.log('login successful');
+        // User Data object to be processed locally and saved as current login data (cleared after logout)
+        setEmail(response.data.userData.email);
+        setFirstName(response.data.userData.firstname);
+        setLastName(response.data.userData.lastname);
+        // TODO: Need to get location from user data
+      }
+    })
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        if (error.response.data.message === 'not logged in')
+          console.log('Not logged in!');
+        else {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    });
 
-
-    const onPressSave = () => {
-        console.log('press save');
-        // TODO: Actually change these fields in database
-    };
+  const onPressSave = () => {
+    console.log('press save');
+    // TODO: Actually change these fields in database
+  };
 
   return (
     <View style={loginSignupStyles.container}>
