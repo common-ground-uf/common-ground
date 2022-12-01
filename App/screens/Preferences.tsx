cd21eca308ae5452f716fc9b302388e8a153693e
Hirshey <noah@thehirschfields.com>
2022-12-01 15:19:23 -0500

    allow user to add/save preferences

--- App/screens/Preferences.tsx
+++ App/screens/Preferences.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button, View, StyleSheet } from 'react-native';
 import { loginSignupStyles } from '../styles/LoginSingup';
 import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
+import { SERVER_URI } from '../Config';
+import { Storage } from '../data/Storage';
 
 export const styles = StyleSheet.create({
   next: {
@@ -20,7 +23,8 @@ type PreferencesProps = {
 
 function Preferences(props: PreferencesProps) {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState(['mexican', '$-$$']);
+  const [value, setValue] = React.useState([]);
+  const [oldValue, setOldValue] = React.useState([]);
   const [items, setItems] = React.useState([
     { label: 'American', value: 'american' },
     { label: 'Mexican', value: 'mexican' },
@@ -36,9 +40,65 @@ function Preferences(props: PreferencesProps) {
     { label: '$$$', value: '$$$' },
   ]);
 
-  const onPressNext = () => {
-    props.navigation.navigate('Waiting on Friends');
+  const onPressSave = async () => {
+
+    const profile = await Storage.get('profile');
+    if (!profile) {
+      props.navigation.navigate('Login');
+      return;
+    }
+    const profileInfo = JSON.parse(profile);
+    let preferencesToAdd : string[] = [];
+    let preferencesToRemove : string[] = [];
+    for(let i = 0; i < value.length; i++) {
+      if(!oldValue.includes(value[i])) {
+        preferencesToAdd.push(value[i]);
+      }
+    }
+    for(let i = 0; i < oldValue.length; i++) {
+      if(!value.includes(oldValue[i])) {
+        preferencesToRemove.push(oldValue[i]);
+      }
+    }
+    console.log("adding: " + preferencesToAdd);
+    console.log("removing: " + preferencesToRemove);
+    if(preferencesToAdd.length > 0) {
+      axios.post(`${SERVER_URI}/users/${profileInfo.id}/prefs`, {
+        preferences: preferencesToAdd,
+      });
+    }
+    if(preferencesToRemove.length > 0) {
+      axios.delete(`${SERVER_URI}/users/${profileInfo.id}/prefs`, {
+        data: {
+          preferences: preferencesToRemove
+        }
+      });
+    }
   };
+
+  const onScreenFocus = async () => {
+    console.log('Preferences screen focused');
+    const profile = await Storage.get('profile');
+    if (!profile) {
+      props.navigation.navigate('Login');
+      return;
+    }
+    const profileInfo = JSON.parse(profile);
+
+    axios.get(`${SERVER_URI}/users/${profileInfo.id}/prefs`).then((response) => {
+      console.log(response.data);
+      console.log(response.data.prefs);
+      setValue(response.data.prefs);
+      setOldValue(response.data.prefs);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  React.useEffect(() => {
+    onScreenFocus();
+  }, []);
+
   return (
     <View style={loginSignupStyles.container}>
       <DropDownPicker
@@ -62,7 +122,7 @@ function Preferences(props: PreferencesProps) {
         ]}
       />
       <View style={styles.next}>
-        <Button title='Next' onPress={onPressNext}/>
+        <Button title='Save' onPress={onPressSave}/>
       </View>
     </View>
   );
