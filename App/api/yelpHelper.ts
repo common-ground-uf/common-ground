7ddf01c284bb6c262f9e3e511a48812e577e5086
Ranger Chenore <r2chenore@gmail.com>
2022-12-01 15:01:03 -0500

    New explore page (#94)
    * Fix stuff
    
    * Make explore page actually work
    
    * Fix bug

--- App/api/yelpHelper.ts
+++ App/api/yelpHelper.ts
@@ -1,8 +1,14 @@
 import geoMidpoint from './geographicMidpoint';
 import { getBusinessesByCoordinatesAndCategories, Business } from './yelpApi';
 import { Restaurant } from '../commonTypes';
+import { Storage } from '../data/Storage';
+import axios from 'axios';
+import { SERVER_URI } from '../Config';
+// import react native location
+import GetLocation from 'react-native-get-location';
 
-export async function generateOrderedRestaurantList(locations: Array<{latitude: number, longitude: number}>, categoryAliasLists: Array<Array<string>>, pricePreferences: Array<number>): Promise<Restaurant[]> {
+
+export async function generateOrderedRestaurantList(locations: Array<{latitude: number, longitude: number}>, categoryAliasLists: Array<Array<string>>, pricePreferences: Array<number>, limit: number | undefined = undefined): Promise<Restaurant[]> {
     const midpoint = geoMidpoint(locations);
     
     // concatenate all preference lists into one list
@@ -16,7 +22,7 @@ export async function generateOrderedRestaurantList(locations: Array<{latitude: 
     const pricePreferencesString = pricePreferences.join(',');
 
     // get businesses from Yelp API
-    const businesses: Business[] = await getBusinessesByCoordinatesAndCategories(midpoint.latitude, midpoint.longitude, orderedPreferenceString, pricePreferencesString);
+    const businesses: Business[] = await getBusinessesByCoordinatesAndCategories(midpoint.latitude, midpoint.longitude, orderedPreferenceString, pricePreferencesString, limit);
 
     // convert businesses to Restaurant objects
     const restaurants: Restaurant[] = businesses.map(business => businessToRestaurant(business));
@@ -38,4 +44,48 @@ function businessToRestaurant(business: Business): Restaurant {
         description: business.categories.map(category => category.title).join(', '),
         distanceMiles: business.distance * 0.000621371
     };
+}
+
+export async function generateExploreSections(): Promise<Array<{sectionTitle: string, contentData: any}>> {
+    
+    const getProfileId = async () => {
+        //Get profile info from async storage
+        const profile = await Storage.get('profile');
+        if (profile) {
+          const profileInfo = JSON.parse(profile);
+          return profileInfo.id;
+        }
+    };
+
+    const profileId = await getProfileId();
+
+    let prefs: string[] = await axios.get(SERVER_URI + "/users/" + profileId + "/prefs").then((response) => response.data.prefs);
+
+    // filter out prefs that have dollar signs
+    prefs = prefs.filter((pref) => !pref.includes("$"));
+
+    if (prefs.length >= 3) {
+        prefs = prefs.slice(0, 3);
+    }
+    else {
+        prefs = prefs.concat(["pizza", "bars", "coffee"]);
+        prefs = prefs.slice(0, 3);
+    }
+
+    // let location = await GetLocation.getCurrentPosition({enableHighAccuracy: false, timeout: 15000});
+    let location = {latitude: 29.648292, longitude: -82.345171};
+    let { latitude, longitude } = location;
+
+    // get restaurants for each pref
+    const restaurants: Restaurant[][] = await Promise.all(prefs.map(async (pref) => {
+        const restaurants: Restaurant[] = await generateOrderedRestaurantList([{latitude, longitude}], [[pref]], [1, 2, 3, 4], 10);
+        return restaurants;
+    }));
+
+    return prefs.map((pref, index) => {
+        return {
+            sectionTitle: pref,
+            contentData: restaurants[index]
+        }
+    });
 }
