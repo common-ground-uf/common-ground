0b4b404d2d8cd58bae1e7672560807d4ce153678
Eric Navar <eric.navar@asurion.com>
2022-12-01 23:07:36 -0500

    fix

--- App/screens/Explore.tsx
+++ App/screens/Explore.tsx
@@ -96,7 +96,9 @@ function Explore(props: HomeProps) {
             <Text style={styles.sectionTitle}>{section.sectionTitle}</Text>
             <Button
               onPress={() => {
-                props.navigation.navigate('Restaurant List');
+                props.navigation.navigate('Restaurant List',{
+                  restaurantList: section.contentData
+                });
               }}
               title="See all"
               color="#ff6e6e"
