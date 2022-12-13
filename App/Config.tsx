64840969fe1803ad4c6cfd7ead438b87a87ece93
Ranger Chenore <r2chenore@gmail.com>
2022-12-13 12:41:36 -0500

    Update config even though it shoudn't be here

--- App/Config.tsx
+++ App/Config.tsx
@@ -1,3 +1,3 @@
-export const { SERVER_URI } = { SERVER_URI: 'http://4.236.136.136:3000' };
+export const { SERVER_URI } = { SERVER_URI: 'http://20.172.243.184:3000' };
 export const { YELP_API_KEY } = { YELP_API_KEY: 'abNZbujcIbt3TUR86nLqQzrEqXpr_1AyCcRnswJs9j4N3n3kjFT9DZU7xTyTDumOL1aGAxkgUo6tYrZqHj7ef2ZC-MTU1hK21XF1jNGaTaXYWH15fIaM6ht59W08Y3Yx' };
 export const { YELP_CLIENT_ID } = { YELP_CLIENT_ID: '6tNlfg9hKo4gWpJjxErwUQ' };
